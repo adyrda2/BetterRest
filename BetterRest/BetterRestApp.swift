41141d7c62d21298db8357130fe6d8afe7314a79
Angela Dyrda <adyrda2@gmail.com>
2023-09-04 19:50:22 -0500

    Add BetterRest project

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Angela Dyrda on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
